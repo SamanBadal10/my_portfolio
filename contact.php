287deab2518f1937f32e8fb373880edaf1872f72
Saman Badal <themr.saman10@gmail.com>
2025-05-30 16:22:31 +0545

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+echo "PHP is working.<br>";
 
 // Import PHPMailer classes
 use PHPMailer\PHPMailer\PHPMailer;
